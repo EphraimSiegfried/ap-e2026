b00edf334762cff0ea1e6f080f1414414c05d588
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-09-02 15:48:31 +0200

    week 1: solve "getting started" and "implementing arithmetic"

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,21 @@
 module APL.Eval
-  (
+  ( eval,
+    Val (..),
   )
 where
 
-import APL.AST ()
+import APL.AST
+
+data Val = ValInt Integer
+  deriving (Eq, Show)
+
+getInt :: Val -> Integer
+getInt (ValInt x) = x
+
+eval :: Exp -> Val
+eval (CstInt x) = ValInt x
+eval (Add x y) = ValInt ((getInt (eval x)) + (getInt (eval y)))
+eval (Sub x y) = ValInt ((getInt (eval x)) - (getInt (eval y)))
+eval (Mul x y) = ValInt ((getInt (eval x)) * (getInt (eval y)))
+eval (Div x y) = ValInt ((getInt (eval x)) `div` (getInt (eval y)))
+eval (Pow x y) = ValInt ((getInt (eval x)) ^ (getInt (eval y)))
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,26 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp (..))
+import APL.Eval (Val (..), eval)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testCase "Exp evals to Val" $
+        eval (CstInt 3) @?= ValInt 3,
+      testCase "Addition" $
+        eval (Add (CstInt 2) (CstInt 4)) @?= ValInt 6,
+      testCase "Subtraction" $
+        eval (Sub (CstInt 4) (CstInt 2)) @?= ValInt 2,
+      testCase "Multiplication" $
+        eval (Mul (CstInt 3) (CstInt 4)) @?= ValInt 12,
+      testCase "Power" $
+        eval (Pow (CstInt 3) (CstInt 4)) @?= ValInt 81,
+      testCase "Division" $
+        eval (Div (CstInt 8) (CstInt 3)) @?= ValInt 2,
+      testCase "Nested Calculation" $
+        eval (Mul (Add (CstInt 3) (CstInt 1)) (Sub (CstInt 9) (CstInt 1))) @?= ValInt 32
+    ]
