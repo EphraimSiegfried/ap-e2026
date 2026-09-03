d1347e101f0494930e988c8c37efd985c7f7a721
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-09-03 09:34:56 +0200

    week 1: add error handling

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -12,13 +12,21 @@ data Val = ValInt Integer
 getInt :: Val -> Integer
 getInt (ValInt x) = x
 
-evalBin :: (Integer -> Integer -> Integer) -> Exp -> Exp -> Val
-evalBin op x y = ValInt (op (getInt (eval x)) (getInt (eval y)))
+type Error = String
 
-eval :: Exp -> Val
-eval (CstInt x) = ValInt x
+evalBin :: (Integer -> Integer -> Integer) -> Exp -> Exp -> Either Error Val
+evalBin op x y = case eval x of
+  Left ex -> Left ex
+  Right xv -> case eval y of
+    Left ey -> Left ey
+    Right yv -> Right (ValInt (op (getInt xv) (getInt yv)))
+
+eval :: Exp -> Either Error Val
+eval (CstInt x) = Right (ValInt x)
 eval (Add x y) = evalBin (+) x y
 eval (Sub x y) = evalBin (-) x y
 eval (Mul x y) = evalBin (*) x y
+eval (Div _ (CstInt 0)) = Left "Error: Divide by zero"
 eval (Div x y) = evalBin div x y
+eval (Pow x (CstInt y)) = if y > 0 then evalBin (^) x (CstInt y) else Left "Error: Negative exponent"
 eval (Pow x y) = evalBin (^) x y
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -10,17 +10,23 @@ tests =
   testGroup
     "Evaluation"
     [ testCase "Exp evals to Val" $
-        eval (CstInt 3) @?= ValInt 3,
+        eval (CstInt 3) @?= Right (ValInt 3),
       testCase "Addition" $
-        eval (Add (CstInt 2) (CstInt 4)) @?= ValInt 6,
+        eval (Add (CstInt 2) (CstInt 4)) @?= Right (ValInt 6),
       testCase "Subtraction" $
-        eval (Sub (CstInt 4) (CstInt 2)) @?= ValInt 2,
+        eval (Sub (CstInt 4) (CstInt 2)) @?= Right (ValInt 2),
       testCase "Multiplication" $
-        eval (Mul (CstInt 3) (CstInt 4)) @?= ValInt 12,
+        eval (Mul (CstInt 3) (CstInt 4)) @?= Right (ValInt 12),
       testCase "Power" $
-        eval (Pow (CstInt 3) (CstInt 4)) @?= ValInt 81,
+        eval (Pow (CstInt 3) (CstInt 4)) @?= Right (ValInt 81),
       testCase "Division" $
-        eval (Div (CstInt 8) (CstInt 3)) @?= ValInt 2,
+        eval (Div (CstInt 8) (CstInt 3)) @?= Right (ValInt 2),
       testCase "Nested Calculation" $
-        eval (Mul (Add (CstInt 3) (CstInt 1)) (Sub (CstInt 9) (CstInt 1))) @?= ValInt 32
+        eval (Mul (Add (CstInt 3) (CstInt 1)) (Sub (CstInt 9) (CstInt 1)))
+          @?= Right
+            (ValInt 32),
+      testCase "Divide by zero throws error" $
+        eval (Div (CstInt 9) (CstInt 0)) @?= Left "Error: Divide by zero",
+      testCase "Negative exponent throws error" $
+        eval (Pow (CstInt 9) (CstInt (-1))) @?= Left "Error: Negative exponent"
     ]
